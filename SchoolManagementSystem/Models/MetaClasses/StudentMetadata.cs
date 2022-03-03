1b028d037a3392a9484059bf95e1e11c0a652cbd
mindtreejan218 <mindtreejan218@MINDJAN183>
2022-03-03 22:43:16 +0530

    Add project files.

---
+++ SchoolManagementSystem/Models/MetaClasses/StudentMetadata.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SchoolManagementSystem.Models
+{
+    public class StudentMetadata
+    {
+        [StringLength(50)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [StringLength(50)]
+        [Display(Name = "Middle Name")]
+        public string MiddleName { get; set; }
+
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Date of Enrollment")]
+        public System.DateTime EnrollmentDate { get; set; }
+    }
+
+    [MetadataType(typeof(StudentMetadata))] 
+    public partial class Student
+    {
+
+    }
+}
